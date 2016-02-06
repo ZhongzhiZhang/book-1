68b2955d22a3ca8c1376d4e569d61d4f333a3543
lilyJi <lili.ji@colorado.edu>
2016-02-05 19:35:34 -0700

    working garage hour

--- src/apps/parking/react/components/garage-hours.js
+++ src/apps/parking/react/components/garage-hours.js
@@ -1,12 +1,22 @@
 MyComponents.Hour = React.createClass({
   render: function() {
-    return (
-      <div className="card">
-        <div className="card-content">
-          TODO: This is a component to display an item in the hours array
-          Raw props data is {JSON.stringify(this.props)}
-        </div>
-      </div>
+    if (this.props.hour.BEG==undefined) {var beg = <li>{' '}</li>}
+         else {var beg = <li>{'Start Time: '+ this.props.hour.BEG}</li>}
+       if (this.props.hour.END==undefined) {var end = <li>{' '}</li>}
+         else {var end = <li>{'End Time: '+ this.props.hour.END}</li>}
+       if (this.props.hour.FROM==undefined) {var from = <li>{' '}</li>}
+         else {var from = <li>{'From: '+ this.props.hour.FROM}</li>}
+       if (this.props.hour.TO==undefined) {var to = <li>{' '}</li>}
+         else {var to = <li>{'To: '+ this.props.hour.TO}</li>}
+
+    
+       return (<div className="green darken-4">
+         <div className="card-content white-text">
+          <ul>
+             <li> {beg} {end} {from} {to}</li>
+          </ul>
+         </div>
+       </div>
     );
   }
 });
@@ -19,12 +29,11 @@ MyComponents.GarageHours = React.createClass({
       return <MyComponents.Hour hour={h} key={i}/>
     })
 
-    return (
-      <div className="card">
-        <div className="card-content">
-          TODO: This is a component to display the hours of this garage.
-          Raw props data is {JSON.stringify(this.props)}
 
+    return (
+      <div className="green light-green lighten-2">
+        <div className="card-content">
+          <span className="card-title">Open hours: </span>
           { hours }
 
         </div>
