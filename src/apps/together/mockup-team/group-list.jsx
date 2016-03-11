84c3fa966b3dac57257b466edcb7bcee2ee81fa8
Zach Lamb <zala3440@colorado.edu>
2016-03-11 16:41:34 -0700

    got firebase data into group.html page

--- src/apps/together/mockup-team/group-list.jsx
+++ src/apps/together/mockup-team/group-list.jsx
@@ -1,51 +1,22 @@
-MyComponents.Group = React.createClass({
-
-  render: function() {
-    return (
-      <li>{this.props.groups.id}</li>
-    );
-  }
-
-});
-
 MyComponents.GroupList = React.createClass({
+  mixins: [ReactFireMixin],
+  getInitialState: function() {
+    return {
+      groups: []
+    };
+  },
+  componentWillMount: function() {
+    var ref = new Firebase("https://drinktogether.firebaseio.com/groups/id");
+    this.bindAsArray(ref, "groups");
+  },
   render: function() {
-    var name = this.props.group.id.name(function(s,i){
-      return <MyComponents.Group bar={s} key={i}/>
-    })
-
-    var bars = this.props.group.id.bars(function(s,i){
-      return <MyComponents.Group name={s} key={i}/>
-    })
-
-    var groupSize = this.props.group.id.size(function(s,i){
-      return <MyComponents.Group size={s} key={i}/>
-    })
-
-    var date = this.props.group.id.date(function(s,i){
-      return <MyComponents.Group date={s} key={i}/>
-    })
-
+  var groups = this.state.groups.map(function(group) {
     return (
-      <div className="card">
-        <div className="card-content">
-        <h4>Bar</h4>
-        <ul>
-        {bars}
-        </ul>
-        
-        <h2>Members</h2>
-        <ul>
-        {groupSize}
-        </ul>
-
-        <h2>Date</h2>
-        <ul>
-        {date}
-        </ul>
-
-        </div>
-      </div>
+      <li key={ group['.key'] }>
+        <b>{ group.name }</b> is going to { group.bars } and has {group.size} people with them
+      </li>
     );
-  }
+  });
+  return <ul>{ groups }</ul>;
+}
 });
