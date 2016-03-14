e70fd63cf205c0c421987724cdd79cb1746e0703
usbader <baderk.cs@gmail.com>
2016-03-14 11:07:28 -0600

    List of users

--- src/apps/together/mockup-team/components/group-list.jsx
+++ src/apps/together/mockup-team/components/group-list.jsx
@@ -7,27 +7,35 @@ MyComponents.GroupList = React.createClass({
   },
   componentWillMount: function() {
     var ref = new Firebase("https://drinktogether.firebaseio.com/groups/id");
+    
     this.bindAsArray(ref, "groups");
   },
   render: function() {
   var groups = this.state.groups.map(function(group) {
+
+  var rows = [];
+  Object.keys(group.listOfUsers).forEach(function (listValue) {
+  rows.push(<li key={listValue}>{listValue}</li>) })
+  
     return (
       <tr key={ group['.key'] }>
         <td>{ group.name }</td>
-        <td>{ group.bars }</td> 
-        <td>{ group.size }</td> 
+        <td>{ group.bars }</td>    
+        <td><ul>{ rows   }</ul></td>
         <td>{ group.date }</td>
       </tr>
     );
   });
   return (<table>
+          <tbody>
           <tr>
             <th>Group Name</th>    
             <th>Destination</th>   
-            <th>Size</th>  
+            <th>List of users</th>
             <th>Date</th>
           </tr>
           { groups }
+          </tbody>
         </table>)
 }
 });
