9ec9cf804a251312900c5ae7fddcbe8feed4ecad
CallanFisher <cafi6580@colorado.edu>
2016-03-14 18:08:42 -0600

    working navbar

--- src/apps/together/mockup-team/components/navbar.jsx
+++ src/apps/together/mockup-team/components/navbar.jsx
@@ -8,13 +8,13 @@ MyComponents.NavBar = React.createClass({
                 </a>
                 <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
                 <ul id="nav-mobile" className="left hide-on-med-and-down">
-                    <li><a href="index.html">Chat</a></li>
+                    <li><a href="index.html">Home</a></li>
                     <li><a href="map.html">Map</a></li>
                     <li><a href="group.html">Group</a></li>
                     <li><a href="login.html">Login</a></li>
                 </ul>
                 <ul className="side-nav" id="mobile-demo">
-                    <li><a href="index.html">Chat</a></li>
+                    <li><a href="index.html">Home</a></li>
                     <li><a href="map.html">Map</a></li>
                     <li><a href="group.html">Group</a></li>
                     <li><a href="login.html">Login</a></li>
