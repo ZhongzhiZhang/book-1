e07cd8e1dbfa6231a18f8720d03610571cb38cff
ZhongzhiZhang <zhongzhi.zhang@colorado.edu>
2016-03-26 14:02:49 -0600

    join function

--- src/apps/together/mockup-team/components/group-list.jsx
+++ src/apps/together/mockup-team/components/group-list.jsx
@@ -12,7 +12,6 @@ MyComponents.GroupList = React.createClass({
   },
   render: function() {
   var groups = this.state.groups.map(function(group) {
-
   var rows = [];
   Object.keys(group.listOfUsers).forEach(function (listValue) {
   rows.push(<li key={listValue}>{listValue}</li>) })
@@ -23,6 +22,24 @@ MyComponents.GroupList = React.createClass({
         <td>{ group.bars }</td>    
         <td><ul>{ rows   }</ul></td>
         <td>{ group.date }</td>
+        <td><a onClick={function() {alert(group.groupID); 		
+					var session = window.localStorage["firebase:session::drinktogether"];
+					var seesionObj = JSON.parse(session);
+					var username = seesionObj['google'].displayName;alert(username);
+					
+					var Bar = group.bars;
+    					var ref = new Firebase('https://drinktogether.firebaseio.com/groups/id/' + group.groupID + '/listOfUsers');
+				     	ref.child(username).set({
+       					Bar: { EndTime : group.date , StartTime: group.date }
+    					});
+            					ref.child(person.name).child(randBar).set({
+					EndTime : "18:00" , StartTime: "15:00"
+					});
+				}
+					
+			} 
+			href='map.html' className="secondary-content"><i className="material-icons">send</i></a></td>
+        
       </tr>
     );
   });
@@ -33,9 +50,11 @@ MyComponents.GroupList = React.createClass({
             <th>Destination</th>   
             <th>List of users</th>
             <th>Date</th>
+            <th>Access group</th>
           </tr>
           { groups }
           </tbody>
         </table>)
 }
 });
+
